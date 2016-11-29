5a699d1036961c45ca899ad85e12e74f73a3304a
Steve Kellock <steve@kellock.ca>
2016-11-28 23:42:55 -0500

    Simplifies create action.

--- packages/staplegun/src/action.js
+++ packages/staplegun/src/action.js
@@ -4,63 +4,21 @@ import { isNotString, isBlank, throwWith } from './utils'
 import type { Action, ActionArguments, ActionOptions } from '../types'
 
 /**
- * Validates a type.  Throws an error if doesn't pass.
- *
- * @throws Error
- * @param {*} string - The type to validate.
- */
-function validateType (value: string): void {
-  throwWith('type is not a string', isNotString, value)
-  throwWith('type cannot be blank', isBlank, value)
-}
-
-/**
- * Validates some arguments. Throws an error if there's an issue.
- *
- * @param {ActionArguments} value - The arguments to validate
- */
-function validateArguments (value: ActionArguments): void {
-  throwWith('options are null', isNil, value)
-  throwWith('arguments must be strings', any(isNotString), keys(value))
-}
-
-/**
- * Validates some options. Throws an error if there's an issue.
- *
- * @param {ActionOptions} value - The options to validate
- */
-function validateOptions (value: ActionOptions): void {
-  throwWith('options are null', isNil, value)
-  throwWith('options keys must be strings', any(isNotString), keys(value))
-}
-
-/**
  * Creates an action for the given type.
- *
- * @export
- * @param {string} type - The type of the action.
- * @param {ActionArguments} args - Any additional args.
- * @param {ActionOptions} opts - Any addtional options
- * @returns {Action}
  */
 export function createAction (
   type: string,
-  args?: ActionArguments,
-  opts?: ActionOptions
+  args?: ActionArguments = [],
+  options?: ActionOptions = {}
 ): Action {
-  // default arguments and options
-  const theArguments = args || []
-  const theOptions = opts || {}
-
-  // make sure things are sane
-  validateType(type)
-  validateArguments(theArguments)
-  validateOptions(theOptions)
+  // sanity
+  throwWith('type is not a string', isNotString, type)
+  throwWith('type cannot be blank', isBlank, type)
+  throwWith('options are null', isNil, args)
+  throwWith('arguments must be strings', any(isNotString), keys(args))
+  throwWith('options are null', isNil, options)
+  throwWith('options keys must be strings', any(isNotString), keys(options))
 
   // create the action
-  return {
-    type,
-    arguments: theArguments,
-    options: theOptions
-  }
+  return { type, arguments: args, options }
 }
