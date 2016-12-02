df669c07d6896a83403e74fd62901c7fc235ffe3
Steve Kellock <steve@kellock.ca>
2016-12-02 00:37:53 -0500

    Makes the dist build work.

--- packages/staplegun/rollup.config.js
+++ packages/staplegun/rollup.config.js
@@ -3,11 +3,34 @@ import babel from 'rollup-plugin-babel'
 export default {
   entry: 'src/index.js',
   format: 'cjs',
+  external: [
+    'ramda',
+    'fs-jetpack',
+    'ramdasauce',
+    'autobind-decorator',
+    'chalk',
+    'minimist',
+    'babel-runtime/helpers/createClass',
+    'babel-runtime/helpers/classCallCheck',
+    'babel-runtime/helpers/asyncToGenerator',
+    'babel-runtime/regenerator',
+    'babel-runtime/core-js/json/stringify'
+  ],
   plugins: [
     babel({
       babelrc: false,
-      presets: ['es2015-rollup', 'stage-0'],
-      plugins: ['transform-decorators-legacy', 'transform-flow-strip-types']
+      runtimeHelpers: true,
+      externalHelpers: true,
+      presets: [
+        'es2015-rollup',
+        'stage-0'
+      ],
+      plugins: [
+        'external-helpers',
+        'transform-decorators-legacy',
+        'transform-flow-strip-types',
+        'transform-runtime'
+      ]
     })
   ],
   dest: 'dist/staplegun'
