acf736401ccf9b07bb868edb545c2a0ba2a718a8
Steve Kellock <steve@kellock.ca>
2016-12-08 11:33:02 -0500

    Adds a sample app and fixes require resolution! (#43)

--- packages/gluegun/src/index.js
+++ packages/gluegun/src/index.js
@@ -1,1 +1,20 @@
+// --- Hackasaurus Rex --------------------------------------------------------
+//
+// https://github.com/patrick-steele-idem/app-module-path-node
+// https://gist.github.com/branneman/8048520
+//
+// This adds the node_modules for `gluegun` to the "search path".
+//
+// So, wherever folks have their scripts (plugins or their apps), they will be
+// given a chance to resolve dependencies back up to gluegun's node_modules.
+//
+// I may be going to hell for this, but I'm taking you bastards with me.
+// ----------------------------------------------------------------------------
+require('app-module-path').addPath(`${__dirname}/../node_modules`)
+// ----------------------------------------------------------------------------
+
+// Now... on with the show!
+
+// kick off the CLI
 require('./cli')
+
