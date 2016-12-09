67c11e572cd0352080f043621dcfcfd67ce3d8ca
Steve Kellock <steve@kellock.ca>
2016-12-09 08:19:00 -0500

    Adds separator and eol to filesystem + documentation. (#50)

--- packages/gluegun/src/extensions/filesystem-extension.js
+++ packages/gluegun/src/extensions/filesystem-extension.js
@@ -1,15 +1,18 @@
 const jetpack = require('fs-jetpack')
+const os = require('os')
+const path = require('path')
 
 /**
  * Extensions to filesystem.  Brought to you by fs-jetpack.
  *
- * @param  {Plugin}     plugin  The plugin that triggered.
- * @param  {Command}    command The current command that is running.
- * @param  {RunContext} context The running context.
- * @return {Function}           A function to attach to the context.
+ * @return {Function} A function to attach to the context.
  */
-function attach (plugin, command, context) {
-  return jetpack
+function attach () {
+  const extension = jetpack      // jetpack
+  extension.eol = os.EOL         // end of line marker
+  extension.separator = path.sep // path separator
+
+  return extension
 }
 
 module.exports = attach
