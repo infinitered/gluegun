eaa7cc0e19ba6867dd42928f3d06d5c769074f34
Steve Kellock <steve@kellock.ca>
2016-12-11 09:22:57 -0500

    Switches inquirer to enquirer and saves 5 mb with same api.

--- packages/gluegun/src/core-extensions/prompt-extension.js
+++ packages/gluegun/src/core-extensions/prompt-extension.js
@@ -1,37 +1,22 @@
-const inquirer = require('inquirer')
+const Enquirer = require('enquirer')
 
 /**
- * Provides user input prompts via inquirer.js.
+ * Provides user input prompts via enquirer.js.
  *
  * @return {Function} A function to attach to the context.
  */
 function attach () {
-  // rename the prompt so we don't have prompt.prompt
-  const ask = inquirer.prompt
-
-  // make the separator available
-  const separator = line => new inquirer.Separator(line)
-
-  /**
-   * Asks a question with a yes or no outcome.
-   *
-   * @param  {string} message The message to display
-   * @return {bool}           The user's choice.
-   */
-  async function confirm (message, options = {}) {
-    const answer = await ask({
-      type: 'confirm',
-      name: 'yesOrNo',
-      message: message
-    })
-    return answer.yesOrNo
-  }
-
-  return {
-    ask,
-    separator,
-    confirm
-  }
+  const enquirer = new Enquirer()
+  enquirer.register('list', require('prompt-list'))
+  enquirer.register('rawlist', require('prompt-rawlist'))
+  enquirer.register('confirm', require('prompt-confirm'))
+  enquirer.register('expand', require('prompt-expand'))
+  enquirer.register('checkbox', require('prompt-checkbox'))
+  enquirer.register('radio', require('prompt-radio'))
+  enquirer.register('password', require('prompt-password'))
+  enquirer.register('question', require('prompt-question'))
+  enquirer.register('autocomplete', require('prompt-autocomplete'))
+  return enquirer
 }
 
 module.exports = attach
