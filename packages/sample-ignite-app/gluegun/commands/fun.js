366ee32b8320bfb27478e572f72c759a1a91a547
Steve Kellock <steve@kellock.ca>
2016-12-13 21:43:33 -0500

    Removes the old sample app.

--- packages/sample-ignite-app/gluegun/commands/fun.js
+++
@@ -1,5 +0,0 @@
-// @cliDescription This will print some debug stuff.
-
-module.exports = async function (context) {
-  context.print.debug('you did it!')
-}
--- packages/sample-ignite-app/ignite/commands/web.js
+++
@@ -1,5 +0,0 @@
-// @cliDescription prints out the web site.
-
-module.exports = async function (context) {
-  console.log('hello!')
-}
