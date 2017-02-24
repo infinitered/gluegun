c0d400bd50e218bf29e67eec1238834669dd0bc6
Steve Kellock <steve@kellock.ca>
2017-02-24 08:22:14 -0500

    Sets minimum to 7.6.0 in sniff.js

--- packages/gluegun/sniff.js
+++ packages/gluegun/sniff.js
@@ -4,8 +4,9 @@ const semver = require('semver')
 // the structure to return
 const env = {}
 
+env.nodeMinimum = '7.6.0'
 env.nodeVersion = process.version.replace('v', '')
-env.isNewEnough = semver.satisfies(env.nodeVersion, '>= 7.0.0')
+env.isNewEnough = semver.satisfies(env.nodeVersion, '>= ' + env.nodeMinimum)
 env.hasAsyncAwait = false
 
 // check for the harmony-enabled features
