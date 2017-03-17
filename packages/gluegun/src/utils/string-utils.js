7b83b26278b4bbd699b3f3560392c11caada4fa3
Steve Kellock <steve@kellock.ca>
2017-03-17 11:39:25 -0400

    Swaps lodash library for lodash functions. (#178)
    * Fixes a linting miss.
    
    * Switches lodash lib to lodash functions.
    
    * Adds tests for string-utils.

--- packages/gluegun/src/utils/string-utils.js
+++ packages/gluegun/src/utils/string-utils.js
@@ -1,5 +1,19 @@
 const { always, is, isEmpty, pipe, when } = require('ramda')
-const _ = require('lodash')
+const camelCase = require('lodash.camelcase')
+const kebabCase = require('lodash.kebabcase')
+const snakeCase = require('lodash.snakecase')
+const upperCase = require('lodash.uppercase')
+const lowerCase = require('lodash.lowercase')
+const startCase = require('lodash.startcase')
+const upperFirst = require('lodash.upperfirst')
+const lowerFirst = require('lodash.lowerfirst')
+const pad = require('lodash.pad')
+const padStart = require('lodash.padstart')
+const padEnd = require('lodash.padend')
+const trim = require('lodash.trim')
+const trimStart = require('lodash.trimstart')
+const trimEnd = require('lodash.trimend')
+const repeat = require('lodash.repeat')
 
 /**
  * Is this not a string?
@@ -34,170 +48,13 @@ function identity (value) {
 }
 
 /**
- * Converts the value toCamelCase.
- *
- * @param {string} value The string to convert
- * @returns {string}
- */
-function camelCase (value) {
-  return _.camelCase(value)
-}
-
-/**
- * Converts the value to-kebab-case.
- *
- * @param {string} value The string to convert
- * @returns {string}
- */
-function kebabCase (value) {
-  return _.kebabCase(value)
-}
-
-/**
- * Converts the value to_snake_case.
- *
- * @param {string} value The string to convert
- * @returns {string}
- */
-function snakeCase (value) {
-  return _.snakeCase(value)
-}
-
-/**
- * Converts the value TO UPPER CASE.
- *
- * @param {string} value The string to convert
- * @returns {string}
- */
-function upperCase (value) {
-  return _.upperCase(value)
-}
-
-/**
- * Converts the value to lower case.
- *
- * @param {string} value The string to convert
- * @returns {string}
- */
-function lowerCase (value) {
-  return _.lowerCase(value)
-}
-
-/**
- * Converts the value To start case.
- *
- * @param {string} value The string to convert
- * @returns {string}
- */
-function startCase (value) {
-  return _.startCase(value)
-}
-
-/**
- * Upcases the first character.
- *
- * @param {string} value The string to convert
- * @returns {string}
- */
-function upperFirst (value) {
-  return _.upperFirst(value)
-}
-
-/**
- * Lowercases the first character.
- *
- * @param {string} value The string to convert
- * @returns {string}
- */
-function lowerFirst (value) {
-  return _.lowerFirst(value)
-}
-
-/**
  * Converts the value ToPascalCase.
  *
  * @param {string} value The string to convert
  * @returns {string}
  */
 function pascalCase (value) {
-  return pipe(_.camelCase, _.upperFirst)(value)
-}
-
-/**
- * Pads a string with characters to a given length.
- *
- * @param {string} value The string to pad
- * @param {number} length How big to make this string
- * @param {string} character The character to use for extra space
- * @returns {string}
- */
-function pad (value, length = 0, character = ' ') {
-  return _.pad(value, length, character)
-}
-
-/**
- * Pads the front of a string to a given length.
- *
- * @param {string} value The string to pad
- * @param {number} length How big to make this string
- * @param {string} character The character to use for extra space
- * @returns {string}
- */
-function padStart (value, length = 0, character = ' ') {
-  return _.padStart(value, length, character)
-}
-
-/**
- * Pads the back of a string to a given length.
- *
- * @param {string} value The string to pad
- * @param {number} length How big to make this string
- * @param {string} character The character to use for extra space
- * @returns {string}
- */
-function padEnd (value, length = 0, character = ' ') {
-  return _.padEnd(value, length, character)
-}
-
-/**
- * Removes whitespace from the front & back.
- *
- * @param {string} value The string to trim.
- * @returns {string}
- */
-function trim (value) {
-  return _.trim(value)
-}
-
-/**
- * Removes whitespace from the front.
- *
- * @param {string} value The string to trim.
- * @returns {string}
- */
-function trimStart (value) {
-  return _.trimStart(value)
-}
-
-/**
- * Removes whitespace from the back.
- *
- * @param {string} value The string to trim.
- * @returns {string}
- */
-function trimEnd (value) {
-  return _.trimEnd(value)
-}
-
-/**
- * Repeats a character a number of times.
- *
- * @param {string} value The string to repeat.
- * @param {number} value The number of times to repeat
- * @returns {string}
- */
-function repeat (value, count = 1) {
-  return _.repeat(value, count)
+  return pipe(camelCase, upperFirst)(value)
 }
 
 module.exports = {
--- packages/gluegun/test/utils/string-utils.test.js
+++ packages/gluegun/test/utils/string-utils.test.js
@@ -1,5 +1,24 @@
 const test = require('ava')
-const { isBlank, isNotString } = require('../../src/utils/string-utils')
+const {
+  isBlank,
+  isNotString,
+  camelCase,
+  kebabCase,
+  snakeCase,
+  upperCase,
+  lowerCase,
+  startCase,
+  upperFirst,
+  lowerFirst,
+  pascalCase,
+  pad,
+  padStart,
+  padEnd,
+  trim,
+  trimStart,
+  trimEnd,
+  repeat
+} = require('../../src/utils/string-utils')
 
 test('isBlank', t => {
   t.true(isBlank())
@@ -24,4 +43,80 @@ test('isNotString', t => {
   t.true(isNotString([]))
   t.false(isNotString(''))
   t.false(isNotString('s'))
+})
+
+test('isNotString', t => {
+  t.false(isNotString(''))
+  t.true(isNotString(2))
+  t.true(isNotString(null))
+  t.true(isNotString(undefined))
+  t.true(isNotString([]))
+  t.true(isNotString({}))
+})
+
+test('camelCase', t => {
+  t.is(camelCase('this here'), 'thisHere')
+})
+
+test('kebabCase', t => {
+  t.is(kebabCase('fun times'), 'fun-times')
+  t.is(kebabCase('FunTimes'), 'fun-times')
+})
+
+test('snakeCase', t => {
+  t.is(snakeCase('a b c'), 'a_b_c')
+  t.is(snakeCase('AlwaysBeClosing'), 'always_be_closing')
+})
+
+test('upperCase', t => {
+  t.is(upperCase('lol'), 'LOL')
+})
+
+test('lowerCase', t => {
+  t.is(lowerCase('ROFL'), 'rofl')
+})
+
+test('startCase', t => {
+  t.is(startCase('hello there'), 'Hello There')
+})
+
+test('upperFirst', t => {
+  t.is(upperFirst('hello world'), 'Hello world')
+})
+
+test('lowerFirst', t => {
+  t.is(lowerFirst('BOOM'), 'bOOM')
+})
+
+test('pascalCase', t => {
+  t.is(pascalCase('check it out'), 'CheckItOut')
+  t.is(pascalCase('checkIt-out'), 'CheckItOut')
+})
+
+test('pad', t => {
+  t.is(pad('a', 3), ' a ')
+})
+
+test('padStart', t => {
+  t.is(padStart('a', 3), '  a')
+})
+
+test('padEnd', t => {
+  t.is(padEnd('a', 3), 'a  ')
+})
+
+test('trim', t => {
+  t.is(trim('   sloppy   '), 'sloppy')
+})
+
+test('trimStart', t => {
+  t.is(trimStart('   ! '), '! ')
+})
+
+test('trimEnd', t => {
+  t.is(trimEnd('  !  '), '  !')
+})
+
+test('repeat', t => {
+  t.is(repeat('a', 4), 'aaaa')
 })
