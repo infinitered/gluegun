80fa175297fb8d2e29bc57d715a17c9fc0f0db94
Jamon Holmgren <jamon@infinite.red>
2017-05-22 20:40:25 -0700

    Implements the rest of the patching core extension

--- docs/context-patching.md
+++ docs/context-patching.md
@@ -1,4 +1,4 @@
-A convenience library that allows easy manipulation of files, such as config files.
+A library that allows easy manipulation of files, such as config files. All functions are async.
 
 # update
 
@@ -9,16 +9,49 @@ If the file ends in `.json`, it'll be read in as an object. Return the updated o
 If the file doesn't end in `.json`, you'll receive a string. Return an updated string to write back to the file.
 
 ```javascript
-context.patching.update('config.json', (config) => {
+await context.patching.update('config.json', (config) => {
   config['key'] = 'new value'
   return config
 })
 
-context.patching.update('config.txt', (data) => {
+await context.patching.update('config.txt', (data) => {
   data.replace('Jamon', 'Boss')
   return data
 })
 ```
 
+# append
 
+Appends a string to the given file.
 
+```javascript
+await context.patching.append('config.txt', 'Append this string\n')
+```
+
+# prepend
+
+Prepends a string to the given file.
+
+```javascript
+await context.patching.prepend('config.txt', 'Prepend this string\n')
+```
+
+# replace
+
+Replaces a string in a given file.
+
+```javascript
+await context.patching.replace('config.txt', 'Remove this string\n', 'Replace with this string\n')
+```
+
+# patch
+
+Allows inserting next to, deleting, and replacing strings in a given file. If `insert` is already present in the file, it won't change the file, unless you also pass through `force: true`.
+
+```javascript
+await context.patching.patch('config.txt', { insert: 'Jamon', before: 'Something else' })
+await context.patching.patch('config.txt', { insert: 'Jamon', after: 'Something else' })
+await context.patching.patch('config.txt', { insert: 'Jamon', replace: 'Something else' })
+await context.patching.patch('config.txt', { insert: 'Jamon', replace: 'Something else', force: true })
+await context.patching.patch('config.txt', { delete: 'Something' })
+```
--- packages/gluegun/src/core-extensions/patching-extension.js
+++ packages/gluegun/src/core-extensions/patching-extension.js
@@ -9,7 +9,7 @@ const { isFile } = require('../utils/filesystem-utils')
  */
 function attach (plugin, command, context) {
   /**
-   * Updates a text file or json config file.
+   * Updates a text file or json config file. Async.
    *
    * @param  {string} filename File to be modified.
    * @param  {Function} callback Callback function for modifying the contents of the file.
@@ -39,7 +39,90 @@ function attach (plugin, command, context) {
     return mutatedContents
   }
 
-  return { update }
+  /**
+   * Convenience function for prepending a string to a given file. Async.
+   *
+   * @param {string} filename       File to be prepended to
+   * @param {string} prependedData  String to prepend
+   */
+  async function prepend(filename, prependedData) {
+    return update(filename, (data) => prependedData + data)
+  }
+
+  /**
+   * Convenience function for appending a string to a given file. Async.
+   *
+   * @param {string} filename       File to be appended to
+   * @param {string} appendedData  String to append
+   */
+  async function append(filename, appendedData) {
+    return update(filename, (data) => data + appendedData)
+  }
+
+  /**
+   * Convenience function for replacing a string in a given file. Async.
+   *
+   * @param {string} filename       File to be prepended to
+   * @param {string} replace        String to replace
+   * @param {string} newContent     String to write
+   */
+  async function replace(filename, replace, newContent) {
+    return update(filename, (data) => data.replace(replace, newContent))
+  }
+
+  /**
+   * Conditionally places a string into a file before or after another string,
+   * or replacing another string, or deletes a string. Async.
+   *
+   * @param {string}   filename        File to be patched
+   * @param {Object}   opts            Options
+   * @param {string}   opts.insert     String to be inserted
+   * @param {string}   opts.before     Insert before this string
+   * @param {string}   opts.after      Insert after this string
+   * @param {string}   opts.replace    Replace this string
+   * @param {string}   opts.delete     Delete this string
+   * @param {boolean}  opts.force      Write even if it already exists
+   *
+   * @example
+   *   await context.patching.patch('thing.js', { before: 'bar', insert: 'foo' })
+   *
+   */
+  async function patch(filename, opts={}) {
+    return update(filename, (data) => patchString(data, opts))
+  }
+
+  function patchString(data, opts) {
+    // Already includes string, and not forcing it
+    if (data.includes(opts.insert) && !opts.force) return false
+
+    // delete <string> is the same as replace <string> + insert ''
+    const replaceString = opts.delete || opts.replace
+
+    if (replaceString) {
+      if (!data.includes(replaceString)) {
+        console.warn(`${replaceString} not found`)
+        return false
+      }
+      // Replace matching string with new string or nothing if nothing provided
+      return data.replace(replaceString, `${opts.insert || ''}`)
+    } else {
+      return insertNextToString(data, opts)
+    }
+  }
+
+  function insertNextToString(data, opts) {
+    // Insert before/after a particular string
+    const findString = opts.before || opts.after
+    if (!data.includes(findString)) {
+      console.warn(`${findString} not found`)
+      return false
+    }
+
+    const newContents = opts.after ? `${findString}${opts.insert || ''}` : `${opts.insert || ''}${findString}`
+    return data.replace(findString, newContents)
+  }
+
+  return { update, append, prepend, replace, patch }
 }
 
 module.exports = attach
