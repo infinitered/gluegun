ee0294fdb0c11cf2ee74e7a2ef2ba579d5bf4aef
Jamon Holmgren <jamon@infinite.red>
2017-05-22 20:40:25 -0700

    Implements the rest of the patching core extension

--- packages/gluegun/test/extensions/patching-extension.test.js
+++ packages/gluegun/test/extensions/patching-extension.test.js
@@ -1,35 +1,34 @@
 const test = require('ava')
 const jetpack = require('fs-jetpack')
+const tempWrite = require('temp-write')
 const create = require('../../src/core-extensions/patching-extension')
 
 // const { startsWith } = require('ramdasauce')
 
 const patching = create()
 
-const CONFIG_SOURCE_FILE = 'test/fixtures/patching/config.json'
-const CONFIG_FILE = 'test/fixtures/patching/config-test.json'
-const TEXT_SOURCE_FILE = 'test/fixtures/patching/words.txt'
-const TEXT_FILE = 'test/fixtures/patching/words-test.txt'
-
-const removeTempFiles = async () => {
-  jetpack.remove(CONFIG_FILE)
-  jetpack.remove(TEXT_FILE)
+const CONFIG_STRING = `{
+  "test": "what???",
+  "test2": "never"
 }
+`
 
-test.before(() => {
-  removeTempFiles()
-  jetpack.copy(CONFIG_SOURCE_FILE, CONFIG_FILE)
-  jetpack.copy(TEXT_SOURCE_FILE, TEXT_FILE)
+const TEXT_STRING = `These are some words.
+
+They're very amazing.
+`
+
+test.beforeEach(t => {
+  t.context.textFile = tempWrite.sync(TEXT_STRING)
 })
 
-test.after(removeTempFiles)
-
 test('update - updates a JSON file', async t => {
-  const updated = await patching.update(CONFIG_FILE, (contents) => {
+  const configFile = tempWrite.sync(CONFIG_STRING, '.json')
+  const updated = await patching.update(configFile, (contents) => {
     t.is(typeof contents, 'object')
     t.is(contents.test, 'what???')
     t.is(contents.test2, 'never')
-    
+
     contents.mutated = true
     return contents
   })
@@ -40,38 +39,133 @@ test('update - updates a JSON file', async t => {
   t.is(updated.test2, 'never')
 
   // file was actually written to with the right contents
-  const newContents = await jetpack.read(CONFIG_FILE, 'utf8')
+  const newContents = await jetpack.read(configFile, 'utf8')
   const expectedContents = `{\n  "test": "what???",\n  "test2": "never",\n  "mutated": true\n}`
   t.is(newContents, expectedContents)
 })
 
 test('update - updates a text file', async t => {
-  const updated = await patching.update(TEXT_FILE, (contents) => {
+  const updated = await patching.update(t.context.textFile, (contents) => {
     t.is(contents, `These are some words.\n\nThey're very amazing.\n`)
-    
+
     contents = `These are some different words.\nEven more amazing.\n`
     return contents
   })
 
   // returned the updated object
   t.is(updated, `These are some different words.\nEven more amazing.\n`)
-  
+
   // file was actually written to with the right contents
-  const newContents = await jetpack.read(TEXT_FILE, 'utf8')
+  const newContents = await jetpack.read(t.context.textFile, 'utf8')
   const expectedContents = `These are some different words.\nEven more amazing.\n`
   t.is(newContents, expectedContents)
 })
 
 test('update - cancel updating a file', async t => {
-  const updated = await patching.update(TEXT_FILE, (contents) => {
+  const updated = await patching.update(t.context.textFile, (contents) => {
     return false
   })
 
   // returned false
   t.falsy(updated)
-  
+
   // file was not altered
-  const newContents = await jetpack.read(TEXT_FILE, 'utf8')
+  const newContents = await jetpack.read(t.context.textFile, 'utf8')
   const expectedContents = `These are some words.\n\nThey're very amazing.\n`
+  t.is(newContents, expectedContents)
+})
+
+test('prepend - prepends a text file', async t => {
+  const updated = await patching.prepend(t.context.textFile, 'prepended info\n')
+
+  // returned the updated object
+  t.is(updated, `prepended info\nThese are some words.\n\nThey're very amazing.\n`)
+
+  // file was actually written to with the right contents
+  const newContents = await jetpack.read(t.context.textFile, 'utf8')
+  const expectedContents = `prepended info\nThese are some words.\n\nThey're very amazing.\n`
+  t.is(newContents, expectedContents)
+})
+
+test('append - appends a text file', async t => {
+  const updated = await patching.append(t.context.textFile, 'appended info\n')
+
+  // returned the updated object
+  t.is(updated, `These are some words.\n\nThey're very amazing.\nappended info\n`)
+
+  // file was actually written to with the right contents
+  const newContents = await jetpack.read(t.context.textFile, 'utf8')
+  const expectedContents = `These are some words.\n\nThey're very amazing.\nappended info\n`
+  t.is(newContents, expectedContents)
+})
+
+test('replace - replaces text in a text file', async t => {
+  const updated = await patching.replace(t.context.textFile, 'very amazing', 'replaced info')
+
+  // returned the updated object
+  t.is(updated, `These are some words.\n\nThey're replaced info.\n`)
+
+  // file was actually written to with the right contents
+  const newContents = await jetpack.read(t.context.textFile, 'utf8')
+  const expectedContents = `These are some words.\n\nThey're replaced info.\n`
+  t.is(newContents, expectedContents)
+})
+
+test('patch - replaces text in a text file', async t => {
+  const updated = await patching.patch(t.context.textFile, {
+    replace: 'very amazing',
+    insert: 'patched info'
+  })
+
+  // returned the updated object
+  t.is(updated, `These are some words.\n\nThey're patched info.\n`)
+
+  // file was actually written to with the right contents
+  const newContents = await jetpack.read(t.context.textFile, 'utf8')
+  const expectedContents = `These are some words.\n\nThey're patched info.\n`
+  t.is(newContents, expectedContents)
+})
+
+test('patch - adds text before other text in a text file', async t => {
+  const updated = await patching.patch(t.context.textFile, {
+    before: 'very amazing',
+    insert: 'patched info '
+  })
+
+  // returned the updated object
+  t.is(updated, `These are some words.\n\nThey're patched info very amazing.\n`)
+
+  // file was actually written to with the right contents
+  const newContents = await jetpack.read(t.context.textFile, 'utf8')
+  const expectedContents = `These are some words.\n\nThey're patched info very amazing.\n`
+  t.is(newContents, expectedContents)
+})
+
+test('patch - adds text after other text in a text file', async t => {
+  const updated = await patching.patch(t.context.textFile, {
+    after: 'some words',
+    insert: ' patched info'
+  })
+
+  // returned the updated object
+  t.is(updated, `These are some words patched info.\n\nThey're very amazing.\n`)
+
+  // file was actually written to with the right contents
+  const newContents = await jetpack.read(t.context.textFile, 'utf8')
+  const expectedContents = `These are some words patched info.\n\nThey're very amazing.\n`
+  t.is(newContents, expectedContents)
+})
+
+test('patch - deletes text in a text file', async t => {
+  const updated = await patching.patch(t.context.textFile, {
+    delete: 'some words'
+  })
+
+  // returned the updated object
+  t.is(updated, `These are .\n\nThey're very amazing.\n`)
+
+  // file was actually written to with the right contents
+  const newContents = await jetpack.read(t.context.textFile, 'utf8')
+  const expectedContents = `These are .\n\nThey're very amazing.\n`
   t.is(newContents, expectedContents)
 })
