5570cc7b834239601cb4dc5cc14f4a9aba72f88a
Steve Kellock <steve@kellock.ca>
2017-12-14 12:53:55 -0500

    ✨ Adds more config file variants

--- src/loaders/config-loader.js
+++ src/loaders/config-loader.js
@@ -1,7 +1,21 @@
 const cosmiconfig = require('cosmiconfig')
 
+/**
+ * Loads the config for the app via CosmicConfig by searching in a few places.
+ *
+ * @param {string} name The base name of the config to load.
+ * @param {string} src The directory to look in.
+ */
 function loadConfig (name, src) {
-  const cosmic = cosmiconfig(name, { sync: true }).load(src)
+  const cosmicOpts = {
+    sync: true, // load the config synchronously
+    rcExtensions: true // allow .namerc.yaml and .namerc.json as well
+  }
+
+  // attempt to load
+  const cosmic = cosmiconfig(name, cosmicOpts).load(src)
+
+  // use what we found or fallback to an empty object
   const config = (cosmic && cosmic.config) || {}
   return config
 }
