e7fd24fa285fe6b31f7772d4690d6d0c2ea5b8c5
Jamon Holmgren <jamon@infinite.red>
2017-12-15 23:45:55 -0800

    Adds default version and help commands Some other refactoring as well

--- src/cli/commands/gluegun.test.js
+++
@@ -1,9 +0,0 @@
-const test = require('ava')
-const cli = require('../cli')
-const sinon = require('sinon')
-sinon.stub(console, 'log')
-
-test('runs generate', async t => {
-  const r = await cli([''])
-  t.is(r.result, 'default command')
-})
