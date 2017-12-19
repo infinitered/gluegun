cf4b9f21bb0ad6b3436e670f372fd833605a91a6
Jamon Holmgren <jamon@infinite.red>
2017-12-18 21:18:34 -0800

    Added integration tests and snapshots

--- src/cli/cli.test.js
+++ src/cli/cli.test.js
@@ -1,6 +1,8 @@
 const test = require('ava')
 const cli = require('./cli')
 const sinon = require('sinon')
+const uniqueTempDir = require('unique-temp-dir')
+
 sinon.stub(console, 'log')
 
 test('can start the cli', async t => {
@@ -11,4 +13,36 @@ test('can start the cli', async t => {
 test('cli shows default', async t => {
   const c = await cli([])
   t.is(c.command.name, 'gluegun')
+})
+
+test('can create a new boilerplate cli', async t => {
+  const pwd = process.cwd()
+
+  const tmp = uniqueTempDir({ create: true })
+  process.chdir(tmp)
+
+  const context = await cli('new foo')
+  t.is(context.command.name, 'new')
+
+  const pkg = context.filesystem.read(`${tmp}/foo/package.json`, 'json')
+
+  t.is(pkg.name, 'foo')
+  t.truthy(pkg.private)
+  t.truthy(Object.keys(pkg.dependencies).includes('gluegun'))
+
+  // Install local version of gluegun to test
+  await context.system.run(`cd ${tmp}/foo && npm install ${pwd}`)
+
+  // Try running the help command, see what it does
+  const runCommand = await context.system.run(`${tmp}/foo/bin/foo help`)
+  t.snapshot(runCommand)
+
+  // Try running the generate command, see what it does
+  const genCommand = await context.system.run(`${tmp}/foo/bin/foo g model test`)
+  t.snapshot(genCommand)
+
+  process.chdir(pwd)
+
+  // clean up
+  context.filesystem.remove(`${tmp}/foo`)
 })
