cd942a5aaa322329a0fdb365a989e548a09251c7
Jamon Holmgren <jamon@infinite.red>
2018-01-08 22:52:16 -0800

    imports core commands rather than require

--- src/core-commands/default.ts
+++ src/core-commands/default.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   run: ({ parameters, runtime, print, strings, meta }) => {
     const infoMessage = strings.isBlank(parameters.first)
       ? `Welcome to ${print.colors.cyan(runtime.brand)} CLI version ${meta.version()}!`
