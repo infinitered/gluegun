27e7ce4818616f633c514b1a724e4df44f98a09e
Jamon Holmgren <jamon@infinite.red>
2018-02-03 12:31:31 -0800

    🍊 Tightens up RunContext types - Also changes from a constructor to default values for properties

--- src/domain/run-context.ts
+++ src/domain/run-context.ts
@@ -2,6 +2,7 @@ import { Runtime } from '../runtime/runtime'
 import { Command } from './command'
 import { Options } from './options'
 import { Plugin } from './plugin'
+import { GluegunFilesystem } from '../core-extensions/filesystem-types'
 
 export interface RunContextParameters {
   /* The command arguments as an array. */
@@ -28,7 +29,7 @@ export interface GluegunRunContext {
   // known properties
   result?: any
   error?: any
-  config?: object
+  config?: Options
   parameters: RunContextParameters
   plugin?: Plugin
   command?: Command
@@ -37,7 +38,7 @@ export interface GluegunRunContext {
   runtime?: Runtime
 
   // known extensions
-  filesystem?: any
+  filesystem?: GluegunFilesystem
   http?: any
   meta?: any
   patching?: any
@@ -56,35 +57,13 @@ export interface GluegunRunContext {
 export class RunContext implements GluegunRunContext {
   [key: string]: any
 
-  public result
-  public error
-  public config
-  public parameters
-  public plugin
-  public command
-  public pluginName
-  public commandName
-  public runtime
-
-  constructor() {
-    /**
-     * The result of the run command.
-     */
-    this.result = null
-
-    /**
-     * An error, if any.
-     */
-    this.error = null
-
-    /**
-     * The configuration.  A mashup of defaults + overrides.
-     */
-    this.config = {}
-
-    /**
-     *  The parameters like the command line options and arguments.
-     */
-    this.parameters = {}
-  }
+  public result = null
+  public error = null
+  public config: Options = {}
+  public parameters: RunContextParameters = {}
+  public plugin = null
+  public command = null
+  public pluginName = null
+  public commandName = null
+  public runtime = null
 }
