73dd42d54a386099b23d048baa03344b80a2376a
Jamon Holmgren <jamon@infinite.red>
2018-12-20 11:22:58 -0800

    Upgrade all dependencies (#412)
    * Upgrade apisauce
    
    * Upgrade all deps
    
    * Husky wants a different spot in the package.json
    
    * Husky update
    
    * Enquirer needs to be locked at 1.0.3 due to incompatibilities with 2.0+
    
    * Lock enquirer

--- src/runtime/runtime-extensions.test.ts
+++ src/runtime/runtime-extensions.test.ts
@@ -5,7 +5,10 @@ import { Runtime } from './runtime'
 test('loads the core extensions in the right order', () => {
   const r = new Runtime()
   r.addCoreExtensions()
-  const list = pipe(pluck('name'), join(', '))(r.extensions)
+  const list = pipe(
+    pluck('name'),
+    join(', '),
+  )(r.extensions)
 
   expect(list).toBe('meta, strings, print, filesystem, semver, system, prompt, http, template, patching')
 })
