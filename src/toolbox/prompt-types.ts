b2bc2f11f20c37ce53195ea0da8b8d3d7b86f94d
Horacio Herrera <me@hherrerag.com>
2019-01-13 00:49:16 +0100

    Types proposal for `ask` GluegunPrompt method
    I'm migrating one yeoman generator to use gluegun and I found this a good way to start contributing to it :)
    
    this is the start, hopefully by the end of my migration I can contribute a little more.
    
    It was a little to find all the docs needed to type this properly, if you can point me to the right direction, I can make this typedefs work for every case.
    
    let me know if this is something you are interested in getting some external help, I don't want to commit on this (I'm doing this on my spare time), but I guess this is helpful for everyone and why not contribute right?

--- src/toolbox/prompt-types.ts
+++ src/toolbox/prompt-types.ts
@@ -2,7 +2,19 @@ export interface GluegunPrompt {
   /* Prompts with a confirm message. */
   confirm(message: string): Promise<boolean>
   /* Prompts with a set of questions. */
-  ask(questions: any): any
+  ask(questions: QuestionType): AskResponse
   /* Returns a separator. */
   separator(): string
+}
+
+interface QuestionType {
+  type: string
+  name: string
+  message: string
+  choices?: string[]
+  default?: string
+}
+
+interface AskResponse {
+  [key: string]: string
 }
