6e5619bc47ec79aee9e948f872e7001cdefa5e99
Brian Gebel <brian@th3collective.com>
2019-07-29 21:20:23 -0700

    fix(types): Allow setting a generic for the ask response -- fixes #393 (#564 by @ductiletoaster)

--- src/toolbox/prompt-types.ts
+++ src/toolbox/prompt-types.ts
@@ -4,16 +4,20 @@ const Enquirer = require('enquirer')
 export type GluegunEnquirer = typeof Enquirer
 export const GluegunEnquirer = Enquirer
 
+export interface GluegunAskResponse {
+  [key: string]: string
+}
+
 export interface GluegunPrompt {
   /* Prompts with a confirm message. */
   confirm(message: string, initial?: boolean): Promise<boolean>
   /* Prompts with a set of questions. */
-  ask<T = object>(
+  ask<T = GluegunAskResponse>(
     questions:
       | PromptOptions
       | ((this: GluegunEnquirer) => PromptOptions)
       | (PromptOptions | ((this: GluegunEnquirer) => PromptOptions))[],
-  ): Promise<GluegunAskResponse>
+  ): Promise<T>
   /* Returns a separator. */
   separator(): string
 }
@@ -21,7 +25,3 @@ export interface GluegunPrompt {
 export interface GluegunQuestionChoices extends Choice {}
 
 export type GluegunQuestionType = PromptOptions
-
-export interface GluegunAskResponse {
-  [key: string]: string
-}
