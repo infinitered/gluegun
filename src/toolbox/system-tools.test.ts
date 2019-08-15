1388cb7217b678747b8e2b609b9d3248f9726c89
JP Erasmus <jperasmus11@gmail.com>
2019-08-15 14:57:16 +0200

    fix(toolbox): `run` command potential fall through when rejecting
    * Promise `resolve` will no longer be called when the `system.run()`
    method rejects
    * Strengthened types for `system.run` slightly
    * Added basic tests for `system.run`

--- src/toolbox/system-tools.test.ts
+++ src/toolbox/system-tools.test.ts
@@ -10,3 +10,11 @@ test('which - non-existing package', () => {
   const result = system.which('non-existing-package')
   expect(result).toBe(null)
 })
+
+test('run - should reject if the command does not exist', async () => {
+  await expect(system.run('non-existing-command')).rejects.toThrowError()
+})
+
+test('run - should resolve if the command exists', async () => {
+  await expect(system.run('pwd', { trim: true })).resolves.toBe(process.cwd())
+})
