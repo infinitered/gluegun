6694828dd46f1e6a9e5a8f7bc8de96abcbfe49bd
Jonatan Lins <jonatanpinheiro99@gmail.com>
2019-08-31 12:08:41 -0300

    fix(cli): templates can now have includes (#570)
    When an include was made inside an .ejs template, an error would occur.
    
    The fix for the problem was to pass a filename property with the template path to the ejs.render().
    
    Close #570.

--- src/toolbox/template-tools.ts
+++ src/toolbox/template-tools.ts
@@ -27,6 +27,7 @@ function buildGenerate(toolbox: GluegunToolbox): (opts: Options) => Promise<stri
       config: toolbox && toolbox.config,
       parameters: toolbox && toolbox.parameters,
       props,
+      filename: '',
     }
 
     // add our string tools to the filters available.
@@ -38,6 +39,9 @@ function buildGenerate(toolbox: GluegunToolbox): (opts: Options) => Promise<stri
     const directory = opts.directory ? opts.directory : `${plugin && plugin.directory}/templates`
 
     const pathToTemplate = `${directory}/${template}`
+
+    // add template path to support includes
+    data.filename = pathToTemplate
 
     // bomb if the template doesn't exist
     if (!filesystem.isFile(pathToTemplate)) {
