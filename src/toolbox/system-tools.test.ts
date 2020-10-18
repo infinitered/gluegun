cd1e6f23ddc8d6eac9a95e92094c636269642af5
Jamon Holmgren <jamonholmgren@gmail.com>
2020-10-17 17:50:17 -0700

    Remove unnecessary test line

--- src/toolbox/system-tools.test.ts
+++ src/toolbox/system-tools.test.ts
@@ -12,7 +12,6 @@ test('which - non-existing package', () => {
 })
 
 test('run - should reject if the command does not exist', async () => {
-  await expect(system.run('non-existing-command')).rejects.toThrowError()
   try {
     await system.run('echo "hi" && non-existing-command')
   } catch (e) {
